c72728696f7f4dddf588ab073291fad772ee9589
Steven Crouse <sdcrouse91@gmail.com>
2025-06-23 23:22:53 -0700

    Added a fake database and a route for creating users
    This fake database will be replaced by a real one in a future commit.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 import express from 'express';
 
+import { getUser, addUser } from './db.js';
+
 const app = express();
 const PORT = process.env.port || 5000;
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+    const { username, password } = req.body;
+    const user = getUser(username);
+
+    if (user) {
+        res.status(409).json('A user with this username already exists!');
+        return;
+    }
+    
+    try {
+        addUser(username, password);
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(500).json('Something went wrong. Please try again later.');
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
---
+++ src/db.js
@@ -0,0 +1,15 @@
+const users = [
+    { id: 1, username: 'user1', password: 'password1' },
+    { id: 2, username: 'user2', password: 'password2' },
+    { id: 3, username: 'user3', password: 'password3' },
+    { id: 4, username: 'user4', password: 'password4' },
+];
+
+export function getUser(username) {
+    return users.find(user => user.username === username);
+}
+
+export function addUser(username, password) {
+    const id = users.length;
+    users.push({ id, username, password });
+}
