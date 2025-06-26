f94d41db908671ca1cdba1e973b0c49c35096c4b
Steven Crouse <sdcrouse91@gmail.com>
2025-06-25 18:41:16 -0700

    Added a validator dependency and used it to check for blank usernames and passwords

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
+import { body, matchedData, validationResult } from 'express-validator';
 
 import { getUser, addUser } from './db.js';
 
@@ -8,26 +9,48 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.post('/users', async (req, res) => {
-    const { username, password } = req.body;
-    const user = getUser(username);
-
-    if (user) {
-        res.status(409).json('A user with this username already exists!');
-        return;
-    }
-
-    const saltRounds = parseInt(process.env.SALT_ROUNDS);
+app.post(
+    '/users',
+    body('username').trim().notEmpty().withMessage('Username is required.'),
+    body('password').trim().notEmpty().withMessage('Password is required.'),
+    async (req, res) => {
+        const result = validationResult(req);
+        
+        if (!result.isEmpty()) {
+            let errorMessages = {};
     
-    try {
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        addUser(username, hashedPassword);
-        res.sendStatus(201);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json('Something went wrong. Please try again later.');
+            for (const error of result.errors) {
+                if (error.path in errorMessages) {
+                    errorMessages[error.path].push(error.msg);
+                } else {
+                    errorMessages[error.path] = [error.msg];
+                }
+            }
+
+            res.status(400).json({ errors: errorMessages });
+            return;
+        }
+
+        const { username, password } = matchedData(req);
+        const user = getUser(username);
+
+        if (user) {
+            res.status(409).json('A user with this username already exists!');
+            return;
+        }
+
+        const saltRounds = parseInt(process.env.SALT_ROUNDS);
+        
+        try {
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            addUser(username, hashedPassword);
+            res.sendStatus(201);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json('Something went wrong. Please try again later.');
+        }
     }
-});
+);
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
