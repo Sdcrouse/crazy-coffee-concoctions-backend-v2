50452ff434e9bf322c230e6c84f20e330b07a750
Steven Crouse <sdcrouse91@gmail.com>
2025-06-27 02:09:44 -0700

    Added several validations for the username and password fields

--- src/app.js
+++ src/app.js
@@ -9,9 +9,19 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const userValidations = () => body('username').trim().notEmpty().withMessage('Username is required.');
+const userValidations = () => body('username')
+    .trim().notEmpty().withMessage('Username is required.').bail()
+    .isLength({ min: 8 }).withMessage('Username must be at least eight characters long.')
+    .matches(/^[\w\.]+$/).withMessage('Username should only contain letters, numbers, periods, and underscores.')
+    .matches(/^[a-zA-Z].*[a-zA-Z\d]$/).withMessage('Username must start with a letter and end with a letter or number.');
 
-const passwordValidations = () => body('password').trim().notEmpty().withMessage('Password is required.');
+const passwordValidations = () => body('password')
+    .trim().notEmpty().withMessage('Password is required.').bail()
+    .isStrongPassword().withMessage(
+        'Password is not strong enough. It must be at least 8 characters long and contain one of each of the following: lowercase letters, uppercase letters, numbers, and special characters.'
+    ).bail()
+    .custom((passValue, { req }) => !passValue.includes(req.body.username) && !passValue.toLowerCase().includes('password'))
+        .withMessage("Password must not contain the username or the word 'password'.");
 
 app.post('/users', userValidations(), passwordValidations(),
     async (req, res) => {
