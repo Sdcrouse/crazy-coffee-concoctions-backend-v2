409d9298b6ea9dcd491ec3ff4425df8b873f4249
Steven Crouse <sdcrouse91@gmail.com>
2025-07-29 21:14:22 -0700

    Ordered the list of concoctions by date in descending order and updated the response to only return a message if there are no concoctions

--- src/controllers/concoctionController.js
+++ src/controllers/concoctionController.js
@@ -1,12 +1,12 @@
 const concoctions = [
-    { id: 1, userId: 3, name: 'Mocha', created: '2025-07-20 09:14:25' },
-    { id: 2, userId: 2, name: 'Basic Instant Coffee', created: '2025-07-21 10:11:12' },
-    { id: 3, userId: 5, name: 'Pumpkin Spice Latte', created: '2025-07-21 12:11:10' },
-    { id: 4, userId: 1, name: 'Basic Espresso', created: '2025-07-23 13:01:55' },
-    { id: 5, userId: 2, name: 'Cocoa Cinnamon Coffee', created: '2025-07-25 08:05:03' },
+    { id: 8, userId: 1, name: 'Iced Latte', created: '2025-07-29 15:30:15' },
     { id: 6, userId: 1, name: 'Cappuccino', created: '2025-07-28 14:22:33' },
     { id: 7, userId: 3, name: 'Perfect Dark Roast Coffee', created: '2025-07-28 07:22:56' },
-    { id: 8, userId: 1, name: 'Iced Latte', created: '2025-07-29 15:30:15' }
+    { id: 5, userId: 2, name: 'Cocoa Cinnamon Coffee', created: '2025-07-25 08:05:03' },
+    { id: 4, userId: 1, name: 'Basic Espresso', created: '2025-07-23 13:01:55' },
+    { id: 3, userId: 5, name: 'Pumpkin Spice Latte', created: '2025-07-21 12:11:10' },
+    { id: 2, userId: 2, name: 'Basic Instant Coffee', created: '2025-07-21 10:11:12' },
+    { id: 1, userId: 3, name: 'Mocha', created: '2025-07-20 09:14:25' }
 ];
 
 // TODO (on the database side): Order these by date created descending (i.e. newest first)
@@ -21,9 +21,9 @@ export async function getConcoctions(req, res) {
         status = 200;
 
         if (userConcoctions.length === 0) {
-            res.status(status).json({ status, message: "You don't have any concoctions! Click 'New Concoction' to create one." });
+            res.status(status).json({ status, noConcoctionsMessage: "You don't have any concoctions! Click 'New Concoction' to create one." });
         } else {
-            res.status(status).json({ status, message: "Here are your concoctions!", concoctions: userConcoctions });
+            res.status(status).json({ status, concoctions: userConcoctions });
         }
     } catch (error) {
         console.error(error);
