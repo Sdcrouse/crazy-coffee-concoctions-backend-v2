52907c013532bef653c95d703b7b3300e2666f3a
Steven Crouse <sdcrouse91@gmail.com>
2025-08-07 18:36:00 -0700

    Created a concoctions table and corresponding seeds file
    As noted in the seeds file, for security purposes there will not be any seeds for the users table.

--- src/schema.sql
+++ src/schema.sql
@@ -3,11 +3,22 @@ CREATE DATABASE crazy_coffee_concoctions;
 USE crazy_coffee_concoctions;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS concoctions;
 
 CREATE TABLE users (
-    username VARCHAR(255) PRIMARY KEY,
 	id INTEGER NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) PRIMARY KEY,
     `password` VARCHAR(255) NOT NULL,
     ref_token_version INTEGER NOT NULL DEFAULT 0,
     UNIQUE KEY (id)
+);
+
+CREATE TABLE concoctions (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    instructions TEXT NOT NULL,
+    notes TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
---
+++ src/seeds.sql
@@ -0,0 +1,59 @@
+-- Note: For security purposes, there are no seeds for the users table.
+
+-- Concoctions:
+
+INSERT INTO concoctions (`name`, instructions, notes, user_id)
+VALUES (
+    'Mocha',
+    'Mix well. Serve warm. Enjoy!',
+    'You can substitute water for milk, if you want it to be less sweet.',
+    3
+);
+INSERT INTO concoctions (`name`, instructions, notes, user_id)
+VALUES (
+    'Basic Instant Coffee',
+    'Add instant coffee, sweetener, and creamer to hot water. Mix well and serve.',
+    'You can add in an optional pinch of cinnamon. Very good around the holidays! Just be aware that the cinnamon may clump and sink to the bottom.',
+    2
+);
+INSERT INTO concoctions (`name`, instructions, notes, user_id)
+VALUES (
+    'Pumpkin Spice Latte',
+    'Mix together the hot water, sugar, and pumpkin spice. Then add the creamer and stir. Serve hot.',
+    'It is easier to mix in the pumpkin spice BEFORE the creamer instead of AFTER.',
+    5
+);
+INSERT INTO concoctions (`name`, instructions, user_id)
+VALUES (
+    'Basic Espresso',
+    'Steep coffee in boiling water for 5-10 minutes, or to desired strength. Enjoy!',
+    1
+);
+INSERT INTO concoctions (`name`, instructions, notes, user_id)
+VALUES (
+    'Cocoa Cinnamon Coffee',
+    'Add coffee powder, cocoa powder, sugar, and cinnamon to hot milk. Stir it all together and serve.',
+    'This is REALLY good around Christmastime!',
+    2
+);
+INSERT INTO concoctions (`name`, instructions, notes, user_id)
+VALUES (
+    'Cappuccino',
+    'First, make the espresso by mixing the hot water and coffee. Then, add in the steamed milk. Spoon foamed milk on top and create a fun shape with it. Serve.',
+    'Optionally, add a dusting of cinnamon or cocoa powder on top.',
+    1
+);
+INSERT INTO concoctions (`name`, instructions, notes, user_id)
+VALUES (
+    'Perfect Dark Roast Coffee',
+    'Steep coffee in boiling water for 15 minutes. Dump out the coffee grounds. Add sugar and creamer and mix well. Enjoy!',
+    'I prefer using a French press to limit the coffee grounds at the bottom of the cup.',
+    3
+);
+INSERT INTO concoctions (`name`, instructions, notes, user_id)
+VALUES (
+    'Iced Latte',
+    'Mix together the instant coffee, cold water, sweetener, and creamer. Then carefully add the ice cube to avoid a big splash. Wait about a minute for the coffee to get cold. Enjoy!',
+    'Perfect for a hot summer day!',
+    1
+);
